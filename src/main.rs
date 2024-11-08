b2862da432554e1cc46d73f2c95f32d08040c448
louis-jean0 <louis.jean34@hotmail.fr>
2024-11-08 09:42:13 +0100

    Started boids data structure and few methods

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,49 @@ use bevy::prelude::*;
 fn main() {
     App::new()
     .add_plugins(DefaultPlugins)
+    .add_systems(Startup,spawn_boids)
+    .add_systems(Update, print_boids_types)
     .run();
 }
 
-// pub fn setup(mut commands: Commands) {
-//     commands.spawn()
-// }
+pub fn spawn_boids(mut commands: Commands) {
+    commands.spawn((Boid {
+        boid_type: BoidType::Bird,
+    },
+    Position {
+        position: Vec3::new(0.0,0.0,0.0),
+    },
+    Velocity {
+        velocity: Vec3::new(0.0,0.0,0.0),
+    },
+    Acceleration {
+        acceleration: Vec3::new(0.0,0.0,0.0) 
+    }));
+}
+
+pub fn print_boids_types(boid_query: Query<&Boid>) {
+    for boid in boid_query.iter() {
+        let type_name = match boid.boid_type {
+            BoidType::Bird => "Bird",
+            BoidType::Fish => "Fish"
+        };
+        println!("Boid type : {}", type_name);
+    }
+}
 
 #[derive (Component)]
-pub struct Boid;
-
-#[derive (Component)]
-pub struct Velocity {
-    pub velocity: Vec3
+pub struct Boid {
+    pub boid_type: BoidType
 }
 
 #[derive (Component)]
 pub struct Position {
     pub position: Vec3
+}
+
+#[derive (Component)]
+pub struct Velocity {
+    pub velocity: Vec3
 }
 
 #[derive (Component)]
@@ -31,5 +56,5 @@ pub struct Acceleration {
 #[derive (Debug)]
 pub enum BoidType {
     Bird,
-    Fishc
+    Fish
 }
