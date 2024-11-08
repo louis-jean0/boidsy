f37434430335798e67add9b7fb3cb0bce49bc1c3
louis-jean0 <louis.jean34@hotmail.fr>
2024-11-08 09:03:27 +0100

    Setup project with Rust 1.75.0 and Bevy 0.12.0

--- src/main.rs
+++ src/main.rs
@@ -5,3 +5,31 @@ fn main() {
     .add_plugins(DefaultPlugins)
     .run();
 }
+
+// pub fn setup(mut commands: Commands) {
+//     commands.spawn()
+// }
+
+#[derive (Component)]
+pub struct Boid;
+
+#[derive (Component)]
+pub struct Velocity {
+    pub velocity: Vec3
+}
+
+#[derive (Component)]
+pub struct Position {
+    pub position: Vec3
+}
+
+#[derive (Component)]
+pub struct Acceleration {
+    pub acceleration: Vec3
+}
+
+#[derive (Debug)]
+pub enum BoidType {
+    Bird,
+    Fishc
+}
